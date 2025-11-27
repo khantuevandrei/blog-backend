0447f1c83e0f05fff25d768810978ed46450e2ad
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 12:21:17 +0500

    Modify createUser query to return all fields but the password. Create queries to find user by id & username

--- src/db/queries/userQueries.js
+++ src/db/queries/userQueries.js
@@ -1,18 +1,40 @@
 const pool = require("../pool");
 
 async function createUser(username, password_hash) {
-  const result = await pool.query(`
+  const result = await pool.query(
+    `
     INSERT INTO users (username, password_hash)
     VALUES ($1, $2)
-    RETURNING id
-  `);
-  // Return user id
+    RETURNING id, username, role, created_at
+  `,
+    [username, password_hash]
+  );
   return result.rows[0];
 }
 
-async function findUserByUsername(username) {}
+async function findUserByUsername(username) {
+  const result = await pool.query(
+    `
+    SELECT * FROM users
+    WHERE username = $1
+    LIMIT 1
+  `,
+    [username]
+  );
+  return result.rows[0];
+}
 
-async function findUserById(id) {}
+async function findUserById(id) {
+  const result = await pool.query(
+    `
+    SELECT * FROM users
+    WHERE id = $1
+    LIMIT 1
+    `,
+    [id]
+  );
+  return result.rows[0];
+}
 
 module.exports = {
   createUser,
