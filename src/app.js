343db20843bd5ecda9981fb83174044bd597a073
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 20:11:28 +0500

    Mount routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const authRouter = require("./routes/authRouter");
+const postsRouter = require("./routes/postsRouter");
+const commentsRouter = require("./routes/commentsRouter");
+const passport = require("passport");
 
 const app = express();
 
@@ -8,6 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/comments", commentsRouter);
+
+app.use((req, res) => res.status(404).json({ message: "Route not found" }));
 
 app.get("/", (req, res) => res.send("Welcome"));
 
