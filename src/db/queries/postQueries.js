c0a51f035a7c6ba7105bb715a7d88cc6e0ad8077
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 12:40:35 +0500

    Add updatePost query

--- src/db/queries/postQueries.js
+++ src/db/queries/postQueries.js
@@ -1,3 +1,4 @@
+const { post } = require("../../app");
 const pool = require("../pool");
 
 async function createPost(author_id, title, body) {
@@ -16,11 +17,27 @@ async function publishPost(post_id) {
   const result = await pool.query(
     `
     UPDATE posts
-    SET published = TRUE, published_at = NOW()
+    SET published = TRUE, 
+        published_at = NOW()
     WHERE id = $1
     RETURNING *
   `,
     [post_id]
+  );
+  return result.rows[0];
+}
+
+async function updatePost(post_id, title, body) {
+  const result = await pool.query(
+    `
+    UPDATE posts
+    SET title = COALESCE($2, title),
+        body = COALESCE($3, body),
+        updated_at = NOW()
+    WHERE id = $1
+    RETURNING *
+  `,
+    [post_id, title, body]
   );
   return result.rows[0];
 }
