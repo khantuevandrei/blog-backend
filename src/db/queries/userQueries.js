a620d1427f8eb874132d22b93073c5fe7bb84c22
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 13:47:50 +0500

    Refactor userQueries.
    Return specific fields instead of * in case DB
    columns change later.
    Return null if not found to make it easier for
    controllers

--- src/db/queries/userQueries.js
+++ src/db/queries/userQueries.js
@@ -1,39 +1,41 @@
 const pool = require("../pool");
 
-async function createUser(username, password_hash) {
+async function createUser(username, passwordHash) {
   const result = await pool.query(
     `
     INSERT INTO users (username, password_hash)
     VALUES ($1, $2)
     RETURNING id, username, role, created_at
   `,
-    [username, password_hash]
+    [username, passwordHash]
   );
-  return result.rows[0];
+  return result.rows[0] || null;
 }
 
 async function findUserByUsername(username) {
   const result = await pool.query(
     `
-    SELECT * FROM users
+    SELECT id, username, password_hash, role, created_at
+    FROM users
     WHERE username = $1
     LIMIT 1
   `,
     [username]
   );
-  return result.rows[0];
+  return result.rows[0] || null;
 }
 
 async function findUserById(id) {
   const result = await pool.query(
     `
-    SELECT * FROM users
+    SELECT id, username, role, created_at
+    FROM users
     WHERE id = $1
     LIMIT 1
     `,
     [id]
   );
-  return result.rows[0];
+  return result.rows[0] || null;
 }
 
 module.exports = {
