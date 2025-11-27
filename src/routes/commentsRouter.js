38817be21a5aa118778c4aa15702d4ac93580cd2
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 19:57:28 +0500

    Create commentsRouter & routes

---
+++ src/routes/commentsRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const passport = require("../config/passport-jwt");
+const {
+  getCommentById,
+  createComment,
+  updateComment,
+  deleteComment,
+  getPostComments,
+} = require("../controllers/commentsController");
+
+const router = express.Router();
+
+const authenticateJWT = passport.authenticate("jwt", { session: false });
+
+router.post("/", authenticateJWT, createComment);
+
+router.get("/post/:postId", getPostComments);
+
+router.get("/:commentId", getCommentById);
+
+router.put("/:commentId", authenticateJWT, updateComment);
+
+router.delete("/:commentId", authenticateJWT, deleteComment);
+
+module.exports = router;
