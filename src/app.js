602bfc39bbe19be8b25f35fe030289b36bd14669
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 11:05:26 +0500

    Include cors,helmet & json parser in app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome"));
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-
 require("dotenv").config();
+
+const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
