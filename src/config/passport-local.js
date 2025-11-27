b802542275d33d13612d8c7fd123adcad74b6e10
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 14:12:15 +0500

    Set up passport local strategy

---
+++ src/config/passport-local.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const { findUserByUsername } = require("../db/queries/userQueries");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await findUserByUsername(username);
+        if (!user) return done(null, false, { message: "Incorrect username" });
+
+        const match = await bcrypt.compare(password, user.password_hash);
+        if (!match) return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+module.exports = passport;
