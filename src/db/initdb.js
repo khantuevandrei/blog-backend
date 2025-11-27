0aa306306109ec441a81525728a4e01715321c86
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-27 11:39:12 +0500

    Create initdb script to create required tables

---
+++ src/db/initdb.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+const SQL = `
+  DROP TABLE IF EXISTS comments;
+  DROP TABLE IF EXISTS posts;
+  DROP TABLE IF EXISTS users;
+  
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(255) DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT NOW()
+  );
+  
+  CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    published BOOLEAN DEFAULT false,
+    published_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP
+  );
+
+  CREATE TABLE IF NOT EXISTS comments (
+    id SERIAL PRIMARY KEY,
+    post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    body TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP
+  );
+  `;
+
+async function initdb() {
+  console.log("Seeding...");
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
+  });
+
+  try {
+    await client.connect();
+    console.log("Connected to DB");
+
+    await client.query(SQL);
+    console.log("Tables created successfully");
+  } catch (err) {
+    console.error("Error creating tables", err);
+  } finally {
+    await client.end();
+    console.log("Seeding done");
+  }
+}
+
+initdb();
