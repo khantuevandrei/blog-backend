d3653fceffe09bb2bad03e6f6d95b62fd6421e7c
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-28 18:29:00 +0500

    Add global error handler to app, add comments

--- src/app.js
+++ src/app.js
@@ -8,18 +8,31 @@ const passport = require("passport");
 
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
+// Routes
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/comments", commentsRouter);
 
+// Root route
+app.get("/", (req, res) => res.send("Welcome"));
+
+// Unknown routes
 app.use((req, res) => res.status(404).json({ message: "Route not found" }));
 
-app.get("/", (req, res) => res.send("Welcome"));
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    message: err.message || "Internal server error",
+  });
+});
 
 module.exports = app;
