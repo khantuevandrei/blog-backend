cd19e68a103aae93700ff48b0d4a10a97e126452
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-28 10:22:47 +0500

    Modify getPostById so it fetches and returns an object containing post info, as well as post's author info and post's comments

--- src/db/queries/postsQueries.js
+++ src/db/queries/postsQueries.js
@@ -3,14 +3,56 @@ const pool = require("../pool");
 async function getPostById(postId) {
   const result = await pool.query(
     `
-    SELECT id, author_id, title, body, published_at, created_at, updated_at
-    FROM posts
-    WHERE id = $1
-    LIMIT 1
+    SELECT 
+      p.id,
+      p.title,
+      p.content,
+      p.created_at,
+      p.updated_at,
+
+      u.id AS author_id,
+      u.username AS author_username,
+      u.email AS author_email,
+
+      COALESCE(
+          json_agg(
+              json_build_object(
+                  'id', c.id,
+                  'content', c.content,
+                  'created_at', c.created_at,
+                  'author', json_build_object(
+                      'id', cu.id,
+                      'username', cu.username
+                  )
+              )
+          ) FILTER (WHERE c.id IS NOT NULL),
+          '[]'
+      ) AS comments
+
+    FROM posts p
+    JOIN users u ON p.author_id = u.id
+    LEFT JOIN comments c ON p.id = c.post_id
+    LEFT JOIN users cu ON c.author_id = cu.id
+    WHERE p.id = $1
+    GROUP BY p.id, u.id;
   `,
     [postId]
   );
-  return result.rows[0] || null;
+  const row = result.rows[0];
+  if (!row) return null;
+  return {
+    id: row.id,
+    title: row.title,
+    content: row.content,
+    created_at: row.created_at,
+    updated_at: row.updated_at,
+    author: {
+      id: row.author_id,
+      username: row.author_username,
+      email: row.author_email,
+    },
+    comments: row.comments,
+  };
 }
 
 async function createPost(authorId, title, body) {
