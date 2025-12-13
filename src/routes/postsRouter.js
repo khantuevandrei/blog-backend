d899e56dd3bae625de57d7ed7e94ca8389c8197d
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-13 12:25:52 +0500

    Add optional jwt authenticate for getPostById

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -14,10 +14,23 @@ const router = express.Router();
 
 const authenticateJWT = passport.authenticate("jwt", { session: false });
 
+const optionalAuthenticateJWT = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err) return next(err);
+
+    // user will be undefined if no token or invalid token
+    if (user) {
+      req.user = user;
+    }
+
+    next(); // ALWAYS continue
+  })(req, res, next);
+};
+
 // Posts routes
 router.get("/", getPublishedPosts);
 router.post("/", authenticateJWT, createPost);
-router.get("/:postId", authenticateJWT, getPostById);
+router.get("/:postId", optionalAuthenticateJWT, getPostById);
 router.put("/:postId", authenticateJWT, updatePost);
 router.delete("/:postId", authenticateJWT, deletePost);
 router.patch("/:postId/publish", authenticateJWT, togglePublish);
