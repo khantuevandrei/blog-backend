c87276b6572e7a68ba3bf7e6c376c166c1b282af
Khantuev Andrei <khantuevandrei@gmail.com>
2026-06-26 20:18:25 +0500

    Allow all origins for cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(passport.initialize());
 
